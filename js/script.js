1f9837cb39f06279b033bd8e07d865ea3fe98b69
류솔아 <mav7937@naver.com>
2024-05-23 13:55:32 +0900

    GNB 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 $(function () {
+  // GNB
+  const $header = $('#header');
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu-wrap');
+  const $banner = $('.banner-slide');
+  const $btnMenu = $('.btn-menu');
+  const duration = 100;
+
+  // 마우스가 메뉴에 들어오면(mouseenter)
+  $menu.on('mouseenter', function () {
+    const menuIdx = $(this).index();
+    $menu.removeClass('on').eq(menuIdx).addClass('on');
+
+    openMenu();
+  });
+
+  // 마우스가 메뉴에 나가면(mouseleave)
+  $header.on('mouseleave', function () {
+    $menu.removeClass('on');
+    closeMenu();
+  });
+
+  // 메뉴의 동작을 함수로 정의
+  function openMenu() {
+    $header.addClass('active');
+    $submenu.stop().fadeIn(duration);
+    $banner.stop().fadeIn(duration);
+  }
+  function closeMenu() {
+    $header.removeClass('active');
+    $submenu.stop().fadeOut(duration);
+    $banner.stop().fadeOut(duration);
+  }
+
   // family site
   $('.family-site select').on('change', function () {
     const linkValue = $(this).val();
