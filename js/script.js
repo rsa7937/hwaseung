bd21946fe8543ecd24a1779f1d86c787f743729c
류솔아 <mav7937@naver.com>
2024-05-23 14:34:29 +0900

    메뉴 영역 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $(function () {
   $menu.on('mouseenter', function () {
     const menuIdx = $(this).index();
     $menu.removeClass('on').eq(menuIdx).addClass('on');
+    $submenu.find('li').removeClass('on').eq(menuIdx).addClass('on');
 
     openMenu();
   });
@@ -18,8 +19,13 @@ $(function () {
   // 마우스가 메뉴에 나가면(mouseleave)
   $header.on('mouseleave', function () {
     $menu.removeClass('on');
+    $submenu.find('li').removeClass('on');
+
     closeMenu();
   });
+
+  // 메뉴 버튼을 클릭했을 때
+  $btnMenu.on('click', openMenu);
 
   // 메뉴의 동작을 함수로 정의
   function openMenu() {
