d9e409d0f495a93f58cba7949a43aaa2948b1373
류솔아 <mav7937@naver.com>
2024-05-23 14:59:25 +0900

    스크롤 관련 헤더 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(function () {
   // GNB
+  const $window = $(window);
   const $header = $('#header');
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu-wrap');
@@ -38,6 +39,24 @@ $(function () {
     $submenu.stop().fadeOut(duration);
     $banner.stop().fadeOut(duration);
   }
+
+  // 스크롤 이벤트
+  $window.on('scroll', function () {
+    // 얼마나 스크롤 되었는지 값을 구해서 저장
+    const scrollTop = $(this).scrollTop();
+    // 비주얼 영역의 세로크기 저장
+    const visualHeight = $('.visual').outerHeight();
+    console.log(scrollTop, visualHeight);
+
+    // 두 값을 비교해서 (스크롤깂이 비주얼 영역의 세로보다 크다면 = 비주얼 영역을 지난다.)
+    if (scrollTop >= visualHeight) {
+      // w-bg클래스를 더해줌
+      $header.addClass('w-bg');
+    } else {
+      // 작으면 w-bg클래스를 제거함
+      $header.removeClass('w-bg');
+    }
+  });
 
   // family site
   $('.family-site select').on('change', function () {
